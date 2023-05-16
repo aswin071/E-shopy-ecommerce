bcab05349b9126b377f45e8dc5ff5dd358a78ee9
Aswin <aswinjikz071@gmail.com>
2023-05-17 00:24:26 +0530

    new changes updated

--- eshopy/urls.py
+++ eshopy/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path('', include('carts.urls')),
     path('', include('orders.urls')),
     path('', include('wishlist.urls')),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),    
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='account/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="account/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='account/password_reset_complete.html'), name='password_reset_complete'),    
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 handler404='mainapp.views.error_404'
